0072a59502dd7cb4c8e912d6f333cfb2d8367acc
Kandelsafal <kandelsafal2002@gmail.com>
2026-06-25 08:35:58 +0545

    Transcation Class Setup Code

--- smartBank/models/transcation.py
+++ smartBank/models/transcation.py
@@ -1,3 +1,31 @@
+import uuid
+from datetime import datetime
 
-class Transcation:
-    pass
+
+class Transaction:
+    def __init__(self, amount:float, transaction_type:str, description:str = ""):
+        self.transaction_id = uuid.uuid4()
+        self.timestamp= datetime.now()
+        self.status = "Pending"
+        self.amount = amount
+        self.transaction_type = transaction_type
+        self.description = description
+    
+    def __str__(self):
+        result = (
+            "Transaction Record\n"
+            f"Transaction ID   : {self.transaction_id}\n"
+            f"Timestamp        : {self.timestamp.strftime("%Y-%m-%d %H:%M:%S") }\n"
+            f"Status           : {self.status}\n"
+            f"Amount           : {self.amount}\n"
+            f"Transaction Type : {self.transaction_type}\n"
+            
+        )
+        if self.description :
+            result += f"Description      : {self.description}"
+        
+        return result
+    
+
+trans = Transaction(500, "Deposit", "Salary" )
+print(trans)

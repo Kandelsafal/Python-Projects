d55ef878959a8ad0123d2f0f97e944e7745d264e
Kandelsafal <kandelsafal2002@gmail.com>
2026-06-27 17:33:45 +0545

    Serialization and deserialization of User

--- smartBank/main.py
+++ smartBank/main.py
@@ -1,5 +1,6 @@
 from services.bank import Bank
 from models.account import Account
+from models.user import User
 from utils.enums import AccountType, TransactionType
 
 bank = Bank()
@@ -11,46 +12,62 @@ user = bank.register_user(
     "safal@gmail.com",
     "9800000000"
 )
-
-# Open an account through the Bank
-account = bank.open_account(
-    user.user_id,
-    AccountType.SAVINGS
-)
-bank.deposit(
-    account.account_number,
-    1000,
-    "Initial"
-)
-# ----------------------------
 # Convert to dictionary
-# ----------------------------
-account_data = account.to_dict()
+user_data = user.to_dict()
 
-print("Serialized Account")
-print(account_data)
+print("Dictionary:")
+print(user_data)
 
-# Prepare users dictionary
-# ----------------------------
-users = {
-    user.user_id: user
-}
+# Convert back to User object
+new_user = User.from_dict(user_data)
 
-# Restore Account
-# ----------------------------
-restored_account = Account.from_dict(
-    account_data,
-    users
-)
+print("\nRestored User")
+print("ID:", new_user.user_id)
+print("Name:", new_user.name)
+print("Email:", new_user.email)
+print("Phone:", new_user.phone)
+print("Created:", new_user.created_date)
+print("Accounts:", new_user.accounts)
 
-print("\nRestored Account")
+# # Open an account through the Bank
+# account = bank.open_account(
+#     user.user_id,
+#     AccountType.SAVINGS
+# )
+# bank.deposit(
+#     account.account_number,
+#     1000,
+#     "Initial"
+# )
+# # ----------------------------
+# # Convert to dictionary
+# # ----------------------------
+# account_data = account.to_dict()
 
-print("Account Number :", restored_account.account_number)
-print("Owner          :", restored_account.owner.name)
-print("Balance        :", restored_account.balance)
-print("Status         :", restored_account.status)
-print("Account Type   :", restored_account.account_type)
-print("Transactions   :", len(restored_account.transactions))
+# print("Serialized Account")
+# print(account_data)
+
+# # Prepare users dictionary
+# # ----------------------------
+# users = {
+#     user.user_id: user
+# }
+
+# # Restore Account
+# # ----------------------------
+# restored_account = Account.from_dict(
+#     account_data,
+#     users
+# )
+
+# print("\nRestored Account")
+
+# print("Account Number :", restored_account.account_number)
+# print("Owner          :", restored_account.owner.name)
+# print("Balance        :", restored_account.balance)
+# print("Status         :", restored_account.status)
+# print("Account Type   :", restored_account.account_type)
+# print("Transactions   :", len(restored_account.transactions))
 # # Deposit through the Bank
 # transaction = bank.deposit(
 #     account1.account_number,
--- smartBank/models/user.py
+++ smartBank/models/user.py
@@ -1,15 +1,17 @@
 import uuid
 from datetime import datetime
+from uuid import UUID
 
+from models.account import Account
 
 class User :
-    def __init__(self, name, email, phone):
-        self.__user_id = str(uuid.uuid4())
+    def __init__(self, name:str, email:str , phone:str , accounts = None, user_id:str = None, created_date:datetime = None ):
+        self.__user_id = user_id or str(uuid.uuid4())
         self.__name = name
         self.__email = email
         self.__phone = phone
-        self.__created_date = datetime.now()
-        self.__accounts = []
+        self.__created_date = created_date or datetime.now()
+        self.__accounts = accounts or []
 
     @property
     def user_id(self):
@@ -48,4 +50,31 @@ class User :
     def _add_account(self, account):
         self.__accounts.append(account)
 
-    
+    def to_dict(self):
+        created_date = self.created_date.isoformat()
+
+        accounts = []
+        for account in self.accounts:
+            accounts.append(account.account_number)
+
+        return {
+            "created_date" : created_date,
+            "user_id":self.user_id,
+            "name":self.name,
+            "email":self.email,
+            "phone":self.phone,
+            "accounts":accounts
+        }
+    @classmethod
+    def from_dict(cls, data):
+        created_date = datetime.fromisoformat(data["created_date"])
+        return cls(
+            created_date = created_date,
+            name = data["name"],
+            email = data["email"],
+            phone = data["phone"],
+            user_id = data["user_id"],
+            accounts = []
+
+
+        )
